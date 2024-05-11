4ad9386be4975f198da02c8b49d5d596d1f267f6
opti0 <mcjrdck9@gmail.com>
2024-05-11 13:44:52 +0200

    Pliki z laptopa

---
+++ db.py
@@ -0,0 +1,54 @@
+from flask_sqlalchemy import SQLAlchemy
+
+# Inicjalizacja obiektu SQLAlchemy
+db = SQLAlchemy()
+
+# Model tabeli Users
+class User(db.Model):
+    UserID = db.Column(db.Integer, primary_key=True)
+    Name = db.Column(db.String(255))
+    Email = db.Column(db.String(255))
+    Password = db.Column(db.String(255))
+
+# Model tabeli Signs
+class Sign(db.Model):
+    SignID = db.Column(db.Integer, primary_key=True)
+    SignName = db.Column(db.String(255))
+    Description = db.Column(db.String(2000))
+    QRCode = db.Column(db.String(255))
+    Picture = db.Column(db.LargeBinary)
+
+# Model tabeli Achievements
+class Achievement(db.Model):
+    AchievementID = db.Column(db.Integer, primary_key=True)
+    AchievementName = db.Column(db.String(255))
+    Description = db.Column(db.String(2000))
+    Score = db.Column(db.Integer)
+    Type = db.Column(db.String(255))
+
+# Model tabeli Questions
+class Question(db.Model):
+    QuestionID = db.Column(db.Integer, primary_key=True)
+    QuestionText = db.Column(db.String(255))
+
+# Model tabeli Answers
+class Answer(db.Model):
+    AnswerID = db.Column(db.Integer, primary_key=True)
+    QuestionID = db.Column(db.Integer, db.ForeignKey('question.QuestionID'))
+    AnswerText = db.Column(db.String(255))
+    IsCorrect = db.Column(db.Boolean)
+
+# Model tabeli ConnectingTableQuestions
+class ConnectingTableQuestions(db.Model):
+    SignID = db.Column(db.Integer, db.ForeignKey('sign.SignID'), primary_key=True)
+    QuestionID = db.Column(db.Integer, db.ForeignKey('question.QuestionID'), primary_key=True)
+
+# Model tabeli ConnectingTableSigns
+class ConnectingTableSigns(db.Model):
+    UserID = db.Column(db.Integer, db.ForeignKey('user.UserID'), primary_key=True)
+    SignID = db.Column(db.Integer, db.ForeignKey('sign.SignID'), primary_key=True)
+
+# Model tabeli ConnectingTableAchievements
+class ConnectingTableAchievements(db.Model):
+    UserID = db.Column(db.Integer, db.ForeignKey('user.UserID'), primary_key=True)
+    AchievementID = db.Column(db.Integer, db.ForeignKey('achievement.AchievementID'), primary_key=True)
--- main.py
+++ main.py
@@ -1,6 +1,16 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, session
+from db import *
+import secrets
 
 app = Flask(__name__)
+app.secret_key = secrets.token_hex(16)
+# Konfiguracja połączenia z bazą danych
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+db.init_app(app)
+
+# Tworzenie tablic w bazie danych
+with app.app_context():
+    db.create_all()
 
 # Dummy user database
 users = {'user1': 'password1', 'user2': 'password2'}
@@ -14,7 +24,15 @@ def register():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
-        users[username] = password  # Save user to the database (here it's a simple dictionary)
+        # Sprawdzamy, czy użytkownik już istnieje w bazie danych
+        existing_user = User.query.filter_by(Name=username).first()
+        if existing_user:
+            error_message = "Użytkownik o tej nazwie już istnieje. Proszę wybrać inną nazwę."
+            return render_template('register.html', error_message=error_message)
+        # Jeśli użytkownik nie istnieje, dodajemy go do bazy danych
+        new_user = User(Name=username, Password=password)
+        db.session.add(new_user)
+        db.session.commit()
         return redirect(url_for('login'))
     return render_template('register.html')
 
@@ -23,8 +41,10 @@ def login():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
-        if username in users and users[username] == password:
-            return f"Welcome, {username}!"
+        user = User.query.filter_by(Name=username, Password=password).first()
+        if user and user.Password == password:
+            session['user_id'] = user.UserID
+            return redirect(url_for('index'))
         else:
             error_message = "Nieprawidłowa nazwa użytkownika lub hasło. Spróbuj ponownie!"
             return render_template('login.html', error_message=error_message)
